55dec3e1df26121413e77790bff46e22338fc71e
yaji2k <yaji2k@gmail.com>
2018-10-30 18:05:59 +0300

    edited

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,5 @@
-window.onload = function () {
-    var coordBody = $("html, body");
-    moveUP();
-    function moveUP() {
-        $(".btn_back").on("click", function () {
-            coordBody.animate({scrollTop: 0}, 1000);
-        });
-    }
-}
+$(document).ready(function () {
+    $(".content_box").slick({
+        slidesToShow: 1,
+    });
+});
