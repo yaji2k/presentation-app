ce3426f1e71376b2a175af1df2982a6ddd604cf0
yaji2k <yaji2k@gmail.com>
2019-04-06 09:59:42 +0300

    added adaptive version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    _width = $(window).innerWidth();
+    _breakPoint = 800;
     $('.slider').bxSlider({
         pagerType: 'short',
         nextText: '<span class="arrow right"></span>',
@@ -7,14 +9,41 @@ $(document).ready(function () {
     });
 
     showNav();
-
-    function showNav() {
+    menuSwitch();
+    $(window).on("resize", function () {
+        _width = window.event.target.innerWidth;
+        if(_width > _breakPoint) {
+            $(".menu").css({"display": "flex", left: 0});
+        } else {
+            $(".menu").css({"display": "none", left: "-100%"});
+        }
+        $(".bg_active_menu").css({ "display": "none" });
+    });
+    function showNav(_breakPoint) {
         var item = $(".bx-controls")
-        item.css({"margin-left": "-" + item.width() / 2 + "px"});
+        item.css({ "margin-left": "-" + item.width() / 2 + "px" });
         if ($(".slider").width() === 0) {
             item.css({
                 display: "none"
             });
         }
+    }
+
+    function menuSwitch(_breakPoint) {
+        var _breakPoint = _breakPoint || 800;
+        $(".menu_btn").on("click", function () {
+            if (_width <= _breakPoint) {
+                $(".menu").css({ "display": "flex" }).animate({ left: 0 }, "fast");
+                $(".bg_active_menu").css({ "display": "block" });
+            }
+        });
+        $(".bg_active_menu, .nav_item").on("click", function () {
+            if (_width <= _breakPoint) {
+                $(".menu").animate({ left: "-100%" }, "fast", function () {
+                    $(".menu").css({ "display": "none" });
+                });
+                $(".bg_active_menu").css({ "display": "none" });
+            }
+        });
     }
 });
